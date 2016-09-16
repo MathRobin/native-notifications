72a9b361f90e4719ffa13e7478f2eb270752364f
mrobin <mrobin@companeo.com>
2016-09-16 13:48:58 +0200

    commentaires de code

--- index.js
+++ index.js
@@ -9,14 +9,18 @@ class NativeNotification {
             optsTmp = {};
         }
 
+        // Let's check whether notification permissions have already been granted
         if ('granted' === Notification.permission) {
+            // If it's okay let's create a notification
             notificationObject = new Notification(message, optsTmp);
         } else if ('denied' !== Notification.permission) {
+            // Otherwise, we need to ask the user for permission
             Notification.requestPermission(function (permission) {
                 if (!('permission' in Notification)) {
                     Notification.permission = permission;
                 }
                 if ('granted' === permission) {
+                    // If the user accepts, let's create a notification
                     notificationObject = new Notification(message, optsTmp);
                 }
             });
